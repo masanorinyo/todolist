3a4b3d5baa380c68d8d947b3c25ad29b3b95378e
masanorinyo <masanorinyo@gmail.com>
2014-03-01 09:52:07 -0500

    Initial commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -165,7 +165,8 @@ module.exports = function (grunt) {
         httpFontsPath: '/styles/fonts',
         relativeAssets: false,
         assetCacheBuster: false,
-        raw: 'Sass::Script::Number.precision = 10\n'
+        raw: 'Sass::Script::Number.precision = 10\n',
+        require:'susy'
       },
       dist: {
         options: {
